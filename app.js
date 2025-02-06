6063821c1f6ed8e6f4c67576f78e4df79aabed17
Laudkyle <kyleaby1@gmail.com>
2025-02-06 12:20:21 +0000

    port changes

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ dotenv.config();
 connectDB();
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
