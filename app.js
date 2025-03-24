a21761c0983cebaa8a45e2f3ce06f10fc231b09a
Laudkyle <kyleaby1@gmail.com>
2025-03-24 11:19:35 +0000

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const { v4: uuidv4 } = require("uuid"); // Corrected import
+const cors = require("cors"); // Import CORS
+const { v4: uuidv4 } = require("uuid");
 
 // Import Routes
 const userRoutes = require("./routes/userRoutes");
@@ -21,9 +22,12 @@ const repaymentScheduleRoutes = require("./routes/repaymentScheduleRoutes");
 // Database connection
 const connectDB = require("./config/db");
 
+dotenv.config(); // Load environment variables
+
 const app = express();
 
 // Middleware
+app.use(cors()); // Enable CORS for all origins
 app.use(bodyParser.json());
 
 // Routes
@@ -40,12 +44,12 @@ app.use("/api/momo", momoRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/loans", loanRoutes);
 app.use("/api/repaymentSchedules", repaymentScheduleRoutes);
-dotenv.config();
 
 // Connect to the Database
 connectDB();
 
 // Start the server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
